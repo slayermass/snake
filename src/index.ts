fee918e8d5c7bd79a21e226b9f3543e59e1aaef4
anekhoroshkov@aexsoft.com <anekhoroshkov@aexsoft.com>
2024-06-09 15:14:44 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+window.addEventListener('load', () => {
+  const header = document.createElement('h1');
+  header.innerText = 'Webpack❤️TS';
+
+  const body = document.querySelector('body');
+  body?.appendChild(header);
+});
