7371a7174f7e23d2bdb385d9690d16308727e957
anekhoroshkov@aexsoft.com <anekhoroshkov@aexsoft.com>
2024-06-13 21:49:12 +0700

    start moving snake

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,6 @@
 export const fieldSize: number = 11;
+
 export const snakeStartLength: number = 3;
-export const snakeStartPosition: 'horizontal' | 'vertical' = 'horizontal';
+
+export const snakeStartPosition: 'horizontalTop' | 'horizontalBottom' | 'verticalLeft' | 'verticalRight' =
+  'horizontalTop';
--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,20 @@ const enum BlockType {
   'empty',
   'active',
   'snake',
+  'snakeHead',
 }
 
 const filledBlocks: number[][] = []; // 0 - empty, 1 - active, 2 - snake
+
+const snake: number[][] = [];
+
+const snakeMovingDirection: typeof snakeStartPosition = snakeStartPosition;
+
 let interval: number;
 
 const generateGameField = () => {
   const div = document.createElement('div');
+
   div.className = 'game';
 
   let children = '';
@@ -40,6 +47,8 @@ const render = () => {
 
   const coords = { x: 0, y: -1 };
 
+  const allClasses = ['active', 'snake', 'snakeHead'];
+
   for (let x = 0; x < field.children.length; x += 1) {
     coords.y += 1;
 
@@ -50,6 +59,9 @@ const render = () => {
 
     const elem = field.children[x].classList;
 
+    elem.remove(...allClasses);
+
+    // eslint-disable-next-line default-case
     switch (filledBlocks[coords.x][coords.y]) {
       case BlockType.active:
         elem.add('active');
@@ -57,8 +69,9 @@ const render = () => {
       case BlockType.snake:
         elem.add('snake');
         break;
-      default:
-        elem.remove('active', 'snake');
+      case BlockType.snakeHead:
+        elem.add('snakeHead');
+        break;
     }
   }
 };
@@ -70,18 +83,91 @@ const markBlock = (position: [number, number], type: BlockType) => {
   render();
 };
 
+const syncSnakeWithGameField = () => {
+  console.log('syncSnakeWithGameField', filledBlocks, snake);
+
+  for (let i = 0; i < snake.length; i += 1) {
+    filledBlocks[snake[i][0]][snake[i][1]] = i === 0 ? BlockType.snakeHead : BlockType.snake;
+  }
+};
+
+/**
+ * better name?
+ */
+const getAddition = (pos: 'x' | 'y', number: number, direction: typeof snakeStartPosition = snakeMovingDirection) => {
+  if (pos === 'x' && direction === 'horizontalTop') {
+    return number;
+  }
+  if (pos === 'x' && direction === 'horizontalBottom') {
+    return -number;
+  }
+  if (pos === 'y' && direction === 'verticalLeft') {
+    return number;
+  }
+  if (pos === 'y' && direction === 'verticalRight') {
+    return -number;
+  }
+
+  return 0;
+};
+
 const generateSnake = () => {
   const approximateCenter = Math.floor((fieldSize - 1) / 2);
 
   for (let i = -1; i < snakeStartLength - 1; i += 1) {
-    markBlock(
-      [
-        approximateCenter + (snakeStartPosition === 'horizontal' ? i : 0),
-        approximateCenter + (snakeStartPosition === 'vertical' ? i : 0),
-      ],
-      BlockType.snake,
-    );
+    snake.push([
+      approximateCenter + getAddition('x', i, snakeStartPosition),
+      approximateCenter + getAddition('y', i, snakeStartPosition),
+    ]);
   }
+
+  syncSnakeWithGameField();
+
+  render();
+};
+
+const moveSnake = (position: 'left' | 'right' | 'top' | 'bottom') => {
+  console.log('moveSnake', position, JSON.parse(JSON.stringify(filledBlocks)), snake);
+
+  // const temp = [];
+  //
+  // for (let x = 0; x < fieldSize; x += 1) {
+  //   for (let y = 0; y < fieldSize; y += 1) {
+  //     if (filledBlocks[x][y] === BlockType.snakeHead) {
+  //       temp.unshift([x, y]);
+  //     } else if (filledBlocks[x][y] === BlockType.snake) {
+  //       // temp.push([x, y]);
+  //     }
+  //   }
+  // }
+
+  // TODO check if head can move
+
+  // move head (top)
+  // if (filledBlocks[tempHead[0] - 1] !== undefined && filledBlocks[tempHead[0] - 1][tempHead[1]] !== undefined) {
+  //   filledBlocks[tempHead[0] - 1][tempHead[1]] = BlockType.snakeHead;
+  //   filledBlocks[tempHead[0]][tempHead[1]] = BlockType.snake;
+  // } else {
+  //   console.error('moved out of the game field');
+  //   return;
+  // }
+
+  // const tail = tempBody.pop();
+  // filledBlocks[tail[0]][tail[1]] = BlockType.empty;
+
+  // render();
+};
+
+const addBlockToSnake = () => {
+  const currentTail = snake.at(-1);
+
+  const newTail = [currentTail[0] + getAddition('x', 1), currentTail[1] + getAddition('y', 1)];
+
+  snake.push(newTail);
+
+  syncSnakeWithGameField();
+
+  render();
 };
 
 const createNewBlockInRandomPlace = () => {
@@ -98,7 +184,7 @@ const createNewBlockInRandomPlace = () => {
 
   if (freeBlocks.length === 0) {
     clearInterval(interval);
-    console.log('end of the game');
+    console.error('end of the game');
   } else {
     markBlock(freeBlocks[getRandomNumber(1, freeBlocks.length) - 1], BlockType.active);
   }
@@ -109,7 +195,16 @@ window.addEventListener('load', () => {
 
   generateSnake();
 
-  interval = window.setInterval(() => {
-    createNewBlockInRandomPlace();
-  }, 1000);
+  // @ts-ignore
+  window.addBlockToSnake = () => addBlockToSnake();
+
+  // @ts-ignore
+  window.moveTop = () => moveSnake('top');
+
+  // @ts-ignore
+  window.moveTop = () => moveSnake('right');
+
+  // interval = window.setInterval(() => {
+  //   createNewBlockInRandomPlace();
+  // }, 1000);
 });
